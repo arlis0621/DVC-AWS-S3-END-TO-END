14faa91ce3d28c78d0a63e2dfb05e126d15d536c
Aditya <mt1221953@maths.iitd.ac.in>
2025-07-07 11:24:39 +0530

    Just before using dvc exp run

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -7,6 +7,9 @@ import json
 import logging
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 
+import yaml
+from dvclive import Live
+
 # Logging configuration
 log_dir='logs'
 os.makedirs(log_dir,exist_ok=True)
@@ -29,6 +32,15 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+def load_params(params_path):
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug(f"Loaded params: {params}, type: {type(params)}")  # Add this line
+        return params
+    except Exception as e:
+        logger.error(f"Error loading parameters from {params_path}: {e}")
+        raise
 
 
 def load_model(file_path):
@@ -82,11 +94,20 @@ def save_metrics(metrics,file_path):
         raise
 def main():
     try:
+        params=load_params('params.yaml')
+        
         clf=load_model('./models/model.pkl')
         test_data=load_data('./data/processed/test_tfidf.csv')
         x_test=test_data.iloc[:,:-1].values
         y_test=test_data.iloc[:,-1].values
         metrics=evaluate_model(clf,x_test,y_test)
+        
+        #save_dvc_exp shows of every run by making true
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy',accuracy_score(y_test, clf.predict(x_test)))
+            live.log_metric('precision', precision_score(y_test, clf.predict(x_test)))
+            live.log_metric('recall', recall_score(y_test, clf.predict(x_test)))
+            live.log_params(params)
         save_metrics(metrics,'reports/metrics.json')
     except Exception as e:
         logger.error(f"Error in main function: {e}")
